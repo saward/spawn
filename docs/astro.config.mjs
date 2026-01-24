3f277ac2b0f9ce3e4dc9d51a3c399c83f6b977d6
Mark Saward <mark.saward@episub.com>
2026-01-24 21:51:13 +1100

    cloudflare config for starlight

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://docs.spawn.dev",
+
   integrations: [
     starlight({
       title: "Spawn",
@@ -30,4 +33,6 @@ export default defineConfig({
       ],
     }),
   ],
+
+  adapter: cloudflare(),
 });
